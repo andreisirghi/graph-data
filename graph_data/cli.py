016a0c01501612ed401588fd0c0664281e53dc0b
Andrei Sîrghi <andrei@eclecticiq.com>
2020-07-10 10:58:03 +0300

    add logger configuration

--- graph_data/cli.py
+++ graph_data/cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import json
+import logging
 import sys
 import io
 import click
@@ -33,7 +34,35 @@ class Namespace():
     pass
 
 
+def init_logger():
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.JSONRenderer()
+        ],
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    logging.basicConfig(
+        format="%(message)s",
+        stream=sys.stdout,
+        level=logging.INFO,
+    )
+
+    logging.getLogger("requests").setLevel(logging.WARNING)
+
+
 def main():
+    init_logger()
     return cli(obj=Namespace())
 
 
